529336956251ef32b55518cb38ed02bf43f2e37e
JatinBansal1224 <jatinbansal1224@gmail.com>
2026-09-22 02:32:34 +0530

    Functioning is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,71 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState("");
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowefd] = useState(false);
+  const [length, setLength] = useState(8);
 
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) string += "0123456789";
+    if (charAllowed) string += "!@#$%^*&*()[]{}";
+
+    for (let i = 0; i < length; i++) {
+      let pos = Math.floor(Math.random() * string.length + 1);
+      pass += string.charAt(pos);
+    }
+    setPassword(pass);
+  }, [numberAllowed, charAllowed, length]);
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed])
   return (
-    <h1>hello</h1>
-  )
+    <div className="main">
+      <h1 className="heading">Password Generator</h1>
+      <div className="container">
+        <div className="pass">
+          <input
+            type="text"
+            value={password}
+            placeholder="Password"
+            className="input_box"
+            readOnly
+          ></input>
+          <button className="btn">copy</button>
+        </div>
+        <input
+          type="range"
+          min={8}
+          max={20}
+          value={length}
+          onChange={(e) => {
+            setLength(e.target.value);
+          }}
+        ></input>
+        <label>Length : {length}</label>
+        <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          onChange={() => {
+            setNumberAllowed((e) => !e);
+          }}
+        ></input>
+        <label>Numbers</label>
+        <input
+          type="checkbox"
+          defaultChecked={charAllowed}
+          onChange={() => {
+            setCharAllowefd((e) => !e);
+          }}
+        ></input>
+        <label>Characters</label>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
